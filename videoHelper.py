e54973971c80a3bbe20954a064ec255f563b18e4
czk <q1054504930@qq.com>
2020-12-18 17:49:11 +0800

    update videoHelper to v4 version

--- videoHelper.py
+++ videoHelper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# version 3
+# version 4
 # developed by zk chen
 import time
 import requests
@@ -14,7 +14,6 @@ sessionid = "yours" #需改成自己的
 # 以下字段不用改，下面的代码也不用改动
 user_id = ""
 
-user_id_url = "https://gsscut.yuketang.cn/edu_admin/check_user_session/"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
     'Content-Type': 'application/json',
@@ -35,24 +34,11 @@ leaf_type = {
     "discussion": 4
 }
 
-id_response = requests.get(url=user_id_url, headers=headers)
-try:
-    user_id = re.search(r'"user_id":(.+?)}', id_response.text).group(1).strip()
-except:
-    print("也许是网路问题，获取不了user_id,请试着重新运行")
-    raise Exception("也许是网路问题，获取不了user_id,请试着重新运行!!! please re-run this program!")
-
 def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
     video_id = str(video_id)
     classroomid = str(classroomid)
     url = "https://gsscut.yuketang.cn/video-log/heartbeat/"
     get_url = "https://gsscut.yuketang.cn/video-log/get_video_watch_progress/?cid="+str(cid)+"&user_id="+user_id+"&classroom_id="+classroomid+"&video_type=video&vtype=rate&video_id=" + str(video_id) + "&snapshot=1&term=latest&uv_id=3078"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
-        'Content-Type': 'application/json',
-        'Cookie': 'csrftoken='+csrftoken+'; sessionid='+sessionid+'; university_id=3078; platform_id=3'
-    }
-
     progress = requests.get(url=get_url, headers=headers)
     if_completed = '0'
     try:
@@ -66,35 +52,13 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
         print(video_name+"，尚未学习，现在开始自动学习")
     video_frame = 0
     val = 0
-    learning_rate = 70
+    learning_rate = 20
     t = time.time()
     timstap = int(round(t * 1000))
     while val != "1.0" and val != '1':
-        data = {
-            "heart_data": [
-                {
-                    "i": 5,
-                    "et": "loadstart",
-                    "p": "web",
-                    "n": "ws",
-                    "lob": "cloud4",
-                    "cp": video_frame,
-                    "fp": 0,
-                    "tp": 0,
-                    "sp": 1,
-                    "ts": str(timstap),
-                    "u": int(user_id),
-                    "uip": "",
-                    "c": cid,
-                    "v": int(video_id),
-                    "skuid": skuid,
-                    "classroomid": classroomid,
-                    "cc": video_id,
-                    "d": 0,
-                    "pg": "4512143_tkqx",
-                    "sq": 1,
-                    "t": "video"
-                },
+        heart_data = []
+        for i in range(50):
+            heart_data.append(
                 {
                     "i": 5,
                     "et": "loadeddata",
@@ -117,56 +81,12 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
                     "pg": "4512143_tkqx",
                     "sq": 2,
                     "t": "video"
-                },
-                {
-                    "i": 5,
-                    "et": "heartbeat",
-                    "p": "web",
-                    "n": "ws",
-                    "lob": "cloud4",
-                    "cp": video_frame,
-                    "fp": 0,
-                    "tp": 0,
-                    "sp": 1,
-                    "ts": str(timstap),
-                    "u": int(user_id),
-                    "uip": "",
-                    "c": cid,
-                    "v": int(video_id),
-                    "skuid": skuid,
-                    "classroomid": classroomid,
-                    "cc": video_id, # 给过的哦
-                    "d": 339.6,
-                    "pg": "4512139_x8qx",
-                    "sq": 11,
-                    "t": "video"
-                },
-                {
-                    "i": 5,
-                    "et": "play",
-                    "p": "web",
-                    "n": "ws",
-                    "lob": "cloud4",
-                    "cp": video_frame,
-                    "fp": 0,
-                    "tp": 0,
-                    "sp": 1,
-                    "ts": str(timstap),
-                    "u": int(user_id),
-                    "uip": "",
-                    "c": cid,
-                    "v": int(video_id),
-                    "skuid": skuid,
-                    "classroomid": classroomid,
-                    "cc": video_id,
-                    "d": 437.1,
-                    "pg": "4512139_11jdz",
-                    "sq": 7,
-                    "t": "video"
                 }
-            ]
-        }
+            )
+            video_frame += learning_rate
+        data = {"heart_data": heart_data}
         r = requests.post(url=url,headers=headers,json=data)
+        print(r.text)
         try:
             delay_time = re.search(r'Expected available in(.+?)second.', r.text).group(1).strip()
             print("由于网络阻塞，万恶的雨课堂，要阻塞" + str(delay_time) + "秒")
@@ -180,23 +100,8 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
         if tmp_rate is None:
             return 0
         val = tmp_rate.group(1)
-        real_last_point = re.search(r'"last_point":(.+?),',progress.text).group(1)
-        if video_frame != int(float(real_last_point)):
-            if_completed = re.search(r'"completed":(.+?),', progress.text).group(1)
-            if if_completed == '1':
-                print("视频" + video_id + " " + video_name + "学习完成！")
-                return 1
-            if learning_rate <= 1:
-                print("视频"+video_id+" "+video_name+"学习失败，先学习下一个！")
-                return 0
-            video_frame -= learning_rate
-            learning_rate -= 1
         print("学习进度为：" + str(float(val)*100) + "%/100%" + " last_point: " + str(video_frame))
-        video_frame += learning_rate
-        if video_frame < 0:
-            video_frame = 1
-            learning_rate = 70
-        time.sleep(0.6)
+        time.sleep(0.7)
     print("视频"+video_id+" "+video_name+"学习完成！")
     return 1
 
@@ -204,71 +109,67 @@ def get_videos_ids(course_name,classroom_id,course_sign):
     get_homework_ids = "https://gsscut.yuketang.cn/mooc-api/v1/lms/learn/course/chapter?cid="+str(classroom_id)+"&term=latest&uv_id=3078&sign="+course_sign
     homework_ids_response = requests.get(url=get_homework_ids, headers=headers)
     homework_json = json.loads(homework_ids_response.text)
-    # homework_ids = []
     homework_dic = {}
     try:
         for i in homework_json["data"]["course_chapter"]:
             for j in i["section_leaf_list"]:
                 if "leaf_list" in j:
                     for z in j["leaf_list"]:
-                        # print(z['leaf_type'], z['name'], z['id'])
                         if z['leaf_type'] == leaf_type["video"]:
-                            # homework_ids.append(z["id"])
                             homework_dic[z["id"]] = z["name"]
                 else:
                     if j['leaf_type'] == leaf_type["video"]:
                         # homework_ids.append(j["id"])
                         homework_dic[j["id"]] = j["name"]
         print(course_name+"共有"+str(len(homework_dic))+"个作业喔！")
-        # print(homework_ids)
         return homework_dic
     except:
         print("fail while getting homework_ids!!! please re-run this program!")
         raise Exception("fail while getting homework_ids!!! please re-run this program!")
 
-
 if __name__ == "__main__":
     your_courses = []
-    course = {}
 
-    # first, need to get classroom_id
+    # 首先要获取用户的个人ID，即user_id,该值在查询用户的视频进度时需要使用
+    user_id_url = "https://gsscut.yuketang.cn/edu_admin/check_user_session/"
+    id_response = requests.get(url=user_id_url, headers=headers)
+    try:
+        user_id = re.search(r'"user_id":(.+?)}', id_response.text).group(1).strip()
+    except:
+        print("也许是网路问题，获取不了user_id,请试着重新运行")
+        raise Exception("也许是网路问题，获取不了user_id,请试着重新运行!!! please re-run this program!")
+
+    # 然后要获取教室id
     get_classroom_id = "https://gsscut.yuketang.cn/mooc-api/v1/lms/user/user-courses/?status=1&page=1&no_page=1&term=latest&uv_id=3078"
     submit_url = "https://gsscut.yuketang.cn/mooc-api/v1/lms/exercise/problem_apply/?term=latest&uv_id=3078"
     classroom_id_response = requests.get(url=get_classroom_id, headers=headers)
     try:
         for ins in json.loads(classroom_id_response.text)["data"]["product_list"]:
-            # print(ins["course_name"])
-            course_name = ins["course_name"]
-            classroom_id = ins["classroom_id"]
-            course_sign = ins["course_sign"]
-            sku_id = ins["sku_id"]
-            cid = ins["course_id"]
             your_courses.append({
-                "course_name": course_name,
-                "classroom_id": classroom_id,
-                "course_sign": course_sign,
-                "sku_id": sku_id,
-                "course_id": cid
+                "course_name": ins["course_name"],
+                "classroom_id": ins["classroom_id"],
+                "course_sign": ins["course_sign"],
+                "sku_id": ins["sku_id"],
+                "course_id": ins["course_id"]
             })
     except Exception as e:
         print("fail while getting classroom_id!!! please re-run this program!")
         raise Exception("fail while getting classroom_id!!! please re-run this program!")
+
+    # 显示用户提示
     for index, value in enumerate(your_courses):
         print("编号："+str(index+1)+" 课名："+str(value["course_name"]))
     number = input("你想刷哪门课呢？请输入编号。输入0表示全部课程都刷一遍\n")
     if int(number)==0:
+        #0 表示全部刷一遍
         for ins in your_courses:
             homework_dic = get_videos_ids(ins["course_name"],ins["classroom_id"], ins["course_sign"])
             for one_video in homework_dic.items():
-                res = one_video_watcher(one_video[0],one_video[1],ins["course_id"],user_id,ins["classroom_id"],ins["sku_id"])
-                if res == 0:
-                    error_dic[one_video[0]] = one_video[1]
-            #try again
-            for one_video in error_dic.items():
-                res = one_video_watcher(one_video[0],one_video[1],ins["course_id"],user_id,ins["classroom_id"],ins["sku_id"])
+                one_video_watcher(one_video[0],one_video[1],ins["course_id"],user_id,ins["classroom_id"],ins["sku_id"])
     else:
+        #指定序号的课程刷一遍
         number = int(number)-1
         homework_dic = get_videos_ids(your_courses[number]["course_name"],your_courses[number]["classroom_id"],your_courses[number]["course_sign"])
         for one_video in homework_dic.items():
-            res = one_video_watcher(one_video[0], one_video[1], your_courses[number]["course_id"], user_id, your_courses[number]["classroom_id"],
+            one_video_watcher(one_video[0], one_video[1], your_courses[number]["course_id"], user_id, your_courses[number]["classroom_id"],
                                 your_courses[number]["sku_id"])
